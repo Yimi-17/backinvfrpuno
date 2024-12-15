5ebfd322b6505cafcd932612269e27d13a62c7ce
Yimi-17 <cguidoyimi@gmail.com>
2024-12-15 13:43:10 -0500

    Add xlsx dependencyt

--- src/db.js
+++ src/db.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql2');
 
+// Usar variables de entorno para la configuración
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', // Cambia esto según tu configuración
-  password: '', // Cambia esto según tu configuración
-  database: 'inventario_imeis',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
+// Conectar a la base de datos
 db.connect((err) => {
   if (err) {
     console.error('Error conectando a la base de datos:', err);
--- src/db2.js
+++ src/db2.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2');
 
-// Utiliza las variables de entorno para la configuración
 const db = mysql.createConnection({
-  host: process.env.DB_HOST, // Dirección del host en la nube
-  user: process.env.DB_USER, // Usuario de la base de datos
-  password: process.env.DB_PASSWORD, // Contraseña de la base de datos
-  database: process.env.DB_NAME, // Nombre de la base de datos
+  host: 'localhost',
+  user: 'root', // Cambia esto según tu configuración
+  password: '', // Cambia esto según tu configuración
+  database: 'inventario_imeis',
 });
 
 db.connect((err) => {
